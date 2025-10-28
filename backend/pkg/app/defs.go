2a4b220ea506af541631cd856874cc5d5faa51f4
elmehdibelfkih <elmehdibelfkih@gmail.com>
2025-10-28 15:04:46 +0100

    merge with server setUp

---
+++ backend/pkg/app/defs.go
@@ -0,0 +1,7 @@
+package app
+
+const (
+	// INTERNAL_SERVER_ERROR_LOG_PATH = ""
+	PORT               = ":8080"
+	SERVER_RUN_MESSAGE = "\033[2mServer running on http://localhost:8080\033[0m"
+)
--- backend/pkg/app/server/server.go
+++ backend/pkg/app/server/server.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	defs "social/pkg/app"
+	middleware "social/pkg/app/dependencies/middleware"
+	signals "social/pkg/app/dependencies/signals"
+)
+
+func StartServer() {
+	server := &http.Server{
+		Addr:    defs.PORT,
+		Handler: middleware.RateLimiterMiddleware(nil, 60, 120),
+	}
+
+	fmt.Println(defs.SERVER_RUN_MESSAGE)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			// db.CloseDB() // todo:
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	signals.HandleSignals(server)
+}
