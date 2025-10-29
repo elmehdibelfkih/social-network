e3464b22b771c8bf3037d96e1c75b26a52393090
el mehdi belfkih <elmehdibelfkih@gmail.com>
2025-10-29 17:04:45 +0100

    merge with task/middleware

--- backend/pkg/utils/chain.go
+++ backend/pkg/utils/chain.go
@@ -1,7 +1,7 @@
 package utils
 
-import "net/http"
+// import "net/http"
 
-type Chain func(next http.HandlerFunc)http.HandlerFunc
+// type Chain func(next http.HandlerFunc)http.HandlerFunc
 
-func MiddlewareChain(f []func(next http.HandlerFunc)http.HandlerFunc)
+// func MiddlewareChain(f []func(next http.HandlerFunc)http.HandlerFunc)
