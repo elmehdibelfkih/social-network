8c7df06a8b4d5223ceea3bfaca674e45ffdbaf6c
bourazza <zakinirvana482@gmail.com>
2025-10-31 17:51:04 +0100

    cros 1.0

--- backend/pkg/app/dependencies/middleware/cors.go
+++ backend/pkg/app/dependencies/middleware/cors.go
@@ -1,1 +1,21 @@
 package middleware
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+        if r.Method == "OPTIONS" {
+            // 3. Send a 204 (no content) and return
+            w.WriteHeader(http.StatusNoContent)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    })
+}
+
