a10d4a79021f71e4bee98ff819e170afaa63f510
unknown <mergaouiarbi@gmail.com>
2025-11-02 00:25:06 +0100

    solid auth

--- backend/pkg/services/auth/queries.go
+++ backend/pkg/services/auth/queries.go
@@ -3,10 +3,11 @@ package auth
 //select
 
 const (
-	SELECT_PASSWORD_SESSION         = `SELECT id, password_hash FROM users WHERE email = ? or nickname = ? or id = ?`
-	SELECT_SESSION_TOKEN_BY_SESSION = `SELECT COUNt(*) FROM sessions WHERE session_token = ?`
-	SELECT_USER_BY_ID               = `SELECT id, email, first_name, last_name, date_of_birth, nickname, about_me, avatar_id
-	FROM users WHERE id = ?`
+	SELECT_PASSWORD_SESSION          = `SELECT id, password_hash FROM users WHERE email = ? or nickname = ? or id = ?`
+	SELECT_SESSION_TOKEN_BY_SESSION  = `SELECT COUNt(*) FROM sessions WHERE session_token = ?`
+	SELECT_USER_BY_ID                = `SELECT id, email, first_name, last_name, date_of_birth, nickname, about_me, avatar_id FROM users WHERE id = ?`
+	SELECT_SESSION_BY_ID             = `SELECT id, user_id, session_token, ip_adress, device, created_at FROM sessions WHERE user_id = ?`
+	SELECT_SESSION_BY_ID_AND_SESSION = `SELECT id, user_id, session_token, ip_adress, device, created_at FROM sessions WHERE user_id = ? AND session_token = ?`
 )
 
 //insert
@@ -25,5 +26,5 @@ const ()
 //delete
 
 const (
-	DELETE_USER_SESSION = `DELETE FROM session WHERE user_id = ?`
+	DELETE_USER_SESSION = `DELETE FROM session WHERE user_id = ? AND session_token = ?`
 )
