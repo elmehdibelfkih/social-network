f8955220bec9864210e2ceba8c7ca74f4d000ddd
ussefbouchraa <ussefbouchraa@gmail.com>
2025-11-09 12:08:54 +0100

    Features: starting the task of Feed by Puting the special endpoints with dependencies files

---
+++ backend/pkg/services/feed/handler.go
@@ -0,0 +1,23 @@
+package feed
+
+import "net/http"
+
+
+
+
+
+func GetFeed (w http.ResponseWriter, r *http.Request){
+
+
+}
+
+
+func GetFeedUser (w http.ResponseWriter, r *http.Request){
+
+
+}
+
+func GetFeedGroup (w http.ResponseWriter, r *http.Request){
+
+
+}
