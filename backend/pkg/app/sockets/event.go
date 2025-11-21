fba80888be96c515e0de84f53b61620304b30127
mergaoui-larabi <mergaouiarbi@gmail.com>
2025-11-21 23:32:39 +0100

    setting up new branch

--- backend/pkg/app/sockets/event.go
+++ backend/pkg/app/sockets/event.go
@@ -1,28 +1,62 @@
 package socket
 
 type Event struct {
-	Type    string  `json:"type"`
-	Payload Message `json:"payload"`
+	Type    string        `json:"type"`
+	Payload ClientMessage `json:"payload"`
 }
 
-type Message struct {
-	SenderId     int64    `json:"senderId"`
-	Content      string   `json:"message"`
-	RecipientId  *int64   `json:"recipientId"`
-	GroupId      *int64   `json:"groupId"`
-	CustomTarget *[]int64 `json:"targetIds"`
+type ClientMessage struct {
+	SourceId   int64  `json:"sourceId"`
+	Content    string `json:"message"`
+	ChatId     int64  `json:"chatId"`
+	RecievedAt string `json:"recievedAt"`
 }
 
-func NewEvent(eventType string, senderId int64, content string,
-	recipientId, groupId *int64, customTargets *[]int64) *Event {
-	return &Event{
-		Type: eventType,
-		Payload: Message{
-			SenderId:     senderId,
-			Content:      content,
-			RecipientId:  recipientId,
-			GroupId:      groupId,
-			CustomTarget: customTargets,
-		},
-	}
+type ChatMessage struct {
+	Id        int64  `json:"id"`
+	ChatId    int64  `json:"chatId"`
+	SenderId  int64  `json:"SenderId"`
+	Content   string `json:"content"`
+	SeenState string `json:"seenState"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
 }
+
+type MarkSeen struct {
+	Id       int64 `json:"id"`
+	ChatId   int64 `json:"chatId"`
+	SenderId int64 `json:"SenderId"`
+	// Content   string `json:"content"`
+	SeenState string `json:"seenState"`
+	CreatedAt string `json:"createdAt"`
+	UpdatedAt string `json:"updatedAt"`
+}
+
+type Notification struct {
+	Id           int64  `json:"id"`
+	UserId       int64  `json:"userId"`
+	Type         string `json:"type"`
+	RefrenceId   int64  `json:"refrenceId"`
+	RefrenceType int64  `json:"refrenceType"`
+	Content      string `json:"content"`
+	IsRead       string `json:"isRead"`
+	CreatedAt    string `json:"createdAt"`
+	ReadAt       string `json:"ReadAt"`
+}
+
+type OnlineStatus struct {
+	OnlineUsers []User
+}
+
+type User struct {
+	UserId      int64   `json:"userId"`
+	Email       string  `json:"email"`
+	FirstName   string  `json:"firstName"`
+	LastName    string  `json:"lastName"`
+	DateOfBirth string  `json:"dateOfBirth"`
+	Nickname    *string `json:"nickname"`
+	AboutMe     *string `json:"aboutMe"`
+	AvatarId    *int64  `json:"avatarId"`
+	Online      bool    `json:"online"`
+}
+
