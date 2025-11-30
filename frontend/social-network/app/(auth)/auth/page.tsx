071bf0ce88129f42fe644ae6b9523fe928ef36b2
Your Name <you@example.com>
2025-11-30 13:35:45 +0100

    fix auth

--- frontend/social-network/app/(auth)/auth/page.tsx
+++ frontend/social-network/app/(auth)/auth/page.tsx
@@ -6,16 +6,12 @@ import { AuthProvider } from '@/providers/authProvider';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
         <main>
           <AuthProvider>
             {children}
-            <AddFriends title='' desc='' componentId='' purpose='post' onComplete={() => console.log("Sent group invites.")} />
+            {/* <AddFriends title='' desc='' componentId='' purpose='post' onComplete={() => console.log("Sent group invites.")} /> */}
             <AuthForm />
           </AuthProvider>
         </main>
-      </body>
-    </html>
   );
 }
